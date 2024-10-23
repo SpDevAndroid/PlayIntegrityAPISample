36f71f0a91cc7bb6db8c9ecab8d5029701050162
Shubham.Pundir <shubham.pundir@timesinternet.in>
2024-10-23 13:08:40 +0530

    Composable element updated

--- app/src/main/java/com/spdev/appintegrity/MainActivity.kt
+++ app/src/main/java/com/spdev/appintegrity/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.spdev.appintegrity.ui.theme.AppIntegrityAPISampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +27,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppIntegrityAPISampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ViewStartPlayIntegrityFlow(
+                        name = "User",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ViewStartPlayIntegrityFlow(name: String, modifier: Modifier = Modifier) {
+    val modifierSubtitle = Modifier.padding(16.dp)
+    modifierSubtitle.size(width = 300.dp, height = 100.dp)
+    modifierSubtitle.background(Color.Green)
+    Column {
+        Spacer(modifier = Modifier.size(100.dp))
+        Text(
+            text = "Hello $name",
+            modifier = modifier,
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.size(100.dp))
+        Text(
+            text = "Click here to get play integrity verdict",
+            modifier = modifierSubtitle,
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AppIntegrityAPISampleTheme {
-        Greeting("Android")
+        ViewStartPlayIntegrityFlow(name = "Android")
     }
 }
